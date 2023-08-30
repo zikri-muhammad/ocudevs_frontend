d720dbf6090df93bdd424fcfdcf246d5ac039463
Chairul Affan Adi Putra <caffanap@gmail.com>
2023-08-30 14:51:18 +0700

    Meta Description

--- src/layouts/LandingLayout.js
+++ src/layouts/LandingLayout.js
@@ -1,9 +1,14 @@
 import FeatureNavbar from "@/features/FeatureNavbar";
+import Head from "next/head";
 
 export default function LandingLayout({ children }) {
 
     return (
         <>
+            <Head>
+                <title>Choirul Affan - Portofolio</title>
+                <meta name="description" content="Choirul Affan, Portofolio - Fullstack Programmer from Indonesia" />
+            </Head>
             <FeatureNavbar />
             <main className="min-h-screen bg-white text-gray-800 w-full">
                 {children}
