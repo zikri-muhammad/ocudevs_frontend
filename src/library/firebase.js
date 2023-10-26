64b10fa9c368f6c343cf4de7ec837566c1906ab6
zikri-muhammad <muhammadzikri598@gmail.com>
2023-10-26 18:36:57 +0700

    refactor(gitignore)

--- src/library/firebase.js
+++ src/library/firebase.js
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
@@ -12,6 +12,7 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
+// console.log(firebaseConfig)
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app)
